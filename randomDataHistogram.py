9530198ee60829d04121915e53cf333fb2ac393c
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 12:07:35 +0200

    improve counting algorithm, O(n) instead O(n*11)

--- randomDataHistogram.py
+++ randomDataHistogram.py
@@ -5,21 +5,27 @@ list_length = 20
 
 # populate random_list with 20 random integers from 0 to 10 
 count = 0
-while count < list_length: # used list_length instead of the magic number 20 
+while count < list_length:
 	random_list.append(random.randint(0,10))
 	count += 1
 
 # create a list that contains the count of occurances for each number from 0 to 10 in random_list
 # initialize a count_list to 11 zeros
-count_list = [0] * 11 #NEW FEATURE
+count_list = [0] * 11
 
-# for each number from 0 to 10: count number of occurences in random_list, then add count to count_list at index = number
-for n in range(11):
-	count = 0
-	for e in random_list:
-		if e == n:
-			count += 1
-	count_list[n] = count
+# for each number in random_list: increment element at index = number
+for n in random_list:
+	count_list[n] += 1
+
+"""
+# same solution using while loop:
+
+index = 0
+while index < list_length:
+  n = random_list[index]
+  count_list[n] += 1
+  index += 1
+"""
 
 print random_list
 print count_list
