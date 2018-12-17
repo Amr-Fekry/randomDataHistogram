8b6a555fc865c3d3bad2283525a5a33237afbd2e
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 11:04:05 +0200

    count occurences of number 9 in the list

--- randomDataHistogram.py
+++ randomDataHistogram.py
@@ -1,21 +1,21 @@
 import random
 
-#test randint:
-#print "Random number generated = " + str(random.randint(0,10))
-
 random_list = []
 list_length = 20
 
+# populate random_list with 20 random integers from 0 to 10 
 count = 0
 while count < 20:
 	random_list.append(random.randint(0,10))
 	count += 1
 
-"""
-# better loop without a counter:
-
-while len(random_list) < list_length:
-   random_list.append(random.randint(0,10))
-"""
+# count occurences of number 9 in the list
+index = 0
+nines = 0
+while index < 20:
+	if random_list[index] == 9:
+		nines += 1
+	index += 1
 
 print random_list
+print nines
