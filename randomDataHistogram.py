5f3a704978f1b06f69c92788ff9b3282238c138e
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 10:01:37 +0200

    create list of 20 randints

--- randomDataHistogram.py
+++ randomDataHistogram.py
@@ -1,2 +1,21 @@
-# Let's learn a little bit of Data Analysis and how we can use
-# loops and lists to create, aggregate, and summarize data
+import random
+
+#test randint:
+#print "Random number generated = " + str(random.randint(0,10))
+
+random_list = []
+list_length = 20
+
+count = 0
+while count < 20:
+	random_list.append(random.randint(0,10))
+	count += 1
+
+"""
+# better loop without a counter:
+
+while len(random_list) < list_length:
+   random_list.append(random.randint(0,10))
+"""
+
+print random_list
